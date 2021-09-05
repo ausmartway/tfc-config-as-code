fa5a16046efb73b5b9950acd58cd8d0cdee60850
Yulei Liu <yulei.liu@gmail.com>
2021-09-05 16:09:08 +1000

    fix validation block

--- variables.tf
+++ variables.tf
@@ -3,7 +3,7 @@ variable "github_personal_token" {
   type = string
   description = "The github.com personal token"
    validation {
-    condition     = length(var.github_personal_token) > 4 && substr(var.github_personal_token, 0, 4) == "ghp-"
-    error_message = "The github_personal_token value must be a valid github personal token, starting with \"ghp-\"."
+    condition     = length(var.github_personal_token) > 4 && substr(var.github_personal_token, 0, 4) == "ghp_"
+    error_message = "The github_personal_token value must be a valid github personal token, starting with \"ghp_\"."
   }
 }
