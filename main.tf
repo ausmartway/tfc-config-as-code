1d971e74bdbdd7d80280329b970bbf87e0dac806
Yulei Liu <yulei.liu@gmail.com>
2021-09-10 12:58:58 +1000

    import workspace:tfc-credential-injector

--- main.tf
+++ main.tf
@@ -452,3 +452,30 @@ resource "tfe_oauth_client" "github-b" {
   oauth_token      = var.github_personal_token
   service_provider = "github"
 }
+
+resource "tfe_workspace" "tfc-credential-injector" {
+    allow_destroy_plan            = true
+    auto_apply                    = false
+    description                   = "A workspace that will inject aws and azure credentials to other workspaces automaticly."
+    execution_mode                = "remote"
+    file_triggers_enabled         = false
+    global_remote_state           = false
+
+    name                          = "tfc-credential-injector"
+
+    organization                  = "yulei"
+    queue_all_runs                = false
+    remote_state_consumer_ids     = []
+    speculative_enabled           = true
+    structured_run_output_enabled = true
+    tag_names                     = []
+    terraform_version             = "1.0.6"
+    trigger_prefixes              = []
+
+    vcs_repo {
+        identifier         = "ausmartway/tfc-credential-injector"
+        ingress_submodules = false
+        oauth_token_id     = var.github_personal_token
+    }
+
+}
