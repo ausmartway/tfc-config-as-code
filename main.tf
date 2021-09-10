a957750beeab873264892b8965e006a2fc73620d
Yulei Liu <yulei.liu@gmail.com>
2021-09-10 12:33:47 +1000

    re org code blocks

--- main.tf
+++ main.tf
@@ -36,41 +36,6 @@ resource "tfe_workspace" "aws-s3-demo" {
 
 }
 
-resource "tfe_variable" "aws-s3-demo-aws_access_key_id" {
-  key          = "AWS_ACCESS_KEY_ID"
-  value        = "my_value_name"
-  category     = "env"
-  workspace_id = tfe_workspace.aws-s3-demo.id
-  description  = "AWS Access Key ID"
-  lifecycle {
-    ignore_changes = [value]
-  }
-}
-
-resource "tfe_variable" "aws-s3-demo-aws_secret_access_key" {
-  key          = "AWS_SECRET_ACCESS_KEY"
-  value        = "my_value_name"
-  sensitive    = true
-  category     = "env"
-  workspace_id = tfe_workspace.aws-s3-demo.id
-  description  = "AWS Secret Access Key"
-  lifecycle {
-    ignore_changes = [value]
-  }
-}
-
-resource "tfe_variable" "aws-s3-demo-aws_session_token" {
-  key          = "AWS_SESSION_TOKEN"
-  sensitive    = true
-  value        = "my_value_name"
-  category     = "env"
-  workspace_id = tfe_workspace.aws-s3-demo.id
-  description  = "AWS Session Token"
-  lifecycle {
-    ignore_changes = [value]
-  }
-}
-
 resource "tfe_variable" "aws-s3-demo-aws_region" {
   key          = "AWS_REGION"
   value        = var.aws_default_region
@@ -127,43 +92,6 @@ resource "tfe_workspace" "multi-env-provisioning-example-0-test" {
 }
 
 
-resource "tfe_variable" "multi-env-provisioning-example-0-test-aws_access_key_id" {
-  key          = "AWS_ACCESS_KEY_ID"
-  value        = "my_value_name"
-  sensitive    = true
-  category     = "env"
-  workspace_id = tfe_workspace.multi-env-provisioning-example-0-test.id
-  description  = "AWS Access Key ID"
-  lifecycle {
-    ignore_changes = [value]
-  }
-}
-
-resource "tfe_variable" "multi-env-provisioning-example-0-test-aws_secret_access_key" {
-  key          = "AWS_SECRET_ACCESS_KEY"
-  value        = "my_value_name"
-  sensitive    = true
-  category     = "env"
-  workspace_id = tfe_workspace.multi-env-provisioning-example-0-test.id
-  description  = "AWS Secret Access Key"
-  lifecycle {
-    ignore_changes = [value]
-  }
-}
-
-
-resource "tfe_variable" "multi-env-provisioning-example-0-test-aws_session_token" {
-  key          = "AWS_SESSION_TOKEN"
-  sensitive    = true
-  value        = "my_value_name"
-  category     = "env"
-  workspace_id = tfe_workspace.multi-env-provisioning-example-0-test.id
-  description  = "AWS Session Token"
-  lifecycle {
-    ignore_changes = [value]
-  }
-}
-
 resource "tfe_variable" "multi-env-provisioning-example-0-test-aws_region" {
   key          = "AWS_REGION"
   value        = var.aws_default_region
@@ -200,40 +128,6 @@ resource "tfe_workspace" "multi-env-provisioning-example-1-staging" {
 }
 
 
-resource "tfe_variable" "multi-env-provisioning-example-1-staging-aws_access_key_id" {
-  key          = "AWS_ACCESS_KEY_ID"
-  value        = "my_value_name"
-  category     = "env"
-  workspace_id = tfe_workspace.multi-env-provisioning-example-1-staging.id
-  description  = "AWS Access Key ID"
-  lifecycle {
-    ignore_changes = [value]
-  }
-}
-
-resource "tfe_variable" "multi-env-provisioning-example-1-staging-aws_secret_access_key" {
-  key          = "AWS_SECRET_ACCESS_KEY"
-  value        = "my_value_name"
-  sensitive    = true
-  category     = "env"
-  workspace_id = tfe_workspace.multi-env-provisioning-example-1-staging.id
-  description  = "AWS Secret Access Key"
-  lifecycle {
-    ignore_changes = [value]
-  }
-}
-
-resource "tfe_variable" "multi-env-provisioning-example-1-staging-aws_session_token" {
-  key          = "AWS_SESSION_TOKEN"
-  sensitive    = true
-  value        = "my_value_name"
-  category     = "env"
-  workspace_id = tfe_workspace.multi-env-provisioning-example-1-staging.id
-  description  = "AWS Session Token"
-  lifecycle {
-    ignore_changes = [value]
-  }
-}
 
 resource "tfe_variable" "multi-env-provisioning-example-1-staging-aws_region" {
   key          = "AWS_REGION"
@@ -270,41 +164,6 @@ resource "tfe_workspace" "multi-env-provisioning-example-2-prod" {
 }
 
 
-resource "tfe_variable" "multi-env-provisioning-example-2-prod-aws_access_key_id" {
-  key          = "AWS_ACCESS_KEY_ID"
-  value        = "my_value_name"
-  category     = "env"
-  workspace_id = tfe_workspace.multi-env-provisioning-example-2-prod.id
-  description  = "AWS Access Key ID"
-  lifecycle {
-    ignore_changes = [value]
-  }
-}
-
-resource "tfe_variable" "multi-env-provisioning-example-2-prod-aws_secret_access_key" {
-  key          = "AWS_SECRET_ACCESS_KEY"
-  value        = "my_value_name"
-  sensitive    = true
-  category     = "env"
-  workspace_id = tfe_workspace.multi-env-provisioning-example-2-prod.id
-  description  = "AWS Secret Access Key"
-  lifecycle {
-    ignore_changes = [value]
-  }
-}
-
-resource "tfe_variable" "multi-env-provisioning-example-2-prod-aws_session_token" {
-  key          = "AWS_SESSION_TOKEN"
-  sensitive    = true
-  value        = "my_value_name"
-  category     = "env"
-  workspace_id = tfe_workspace.multi-env-provisioning-example-2-prod.id
-  description  = "AWS Session Token"
-  lifecycle {
-    ignore_changes = [value]
-  }
-}
-
 resource "tfe_variable" "multi-env-provisioning-example-2-prod-aws_region" {
   key          = "AWS_REGION"
   value        = var.aws_default_region
@@ -338,40 +197,6 @@ resource "tfe_workspace" "terraform-aws-vault-demo" {
 }
 
 
-resource "tfe_variable" "terraform-aws-vault-demo-aws_access_key_id" {
-  key          = "AWS_ACCESS_KEY_ID"
-  value        = "my_value_name"
-  category     = "env"
-  workspace_id = tfe_workspace.terraform-aws-vault-demo.id
-  description  = "AWS Access Key ID"
-  lifecycle {
-    ignore_changes = [value]
-  }
-}
-
-resource "tfe_variable" "terraform-aws-vault-demo-aws_secret_access_key" {
-  key          = "AWS_SECRET_ACCESS_KEY"
-  value        = "my_value_name"
-  sensitive    = true
-  category     = "env"
-  workspace_id = tfe_workspace.terraform-aws-vault-demo.id
-  description  = "AWS Secret Access Key"
-  lifecycle {
-    ignore_changes = [value]
-  }
-}
-
-resource "tfe_variable" "terraform-aws-vault-demo-aws_session_token" {
-  key          = "AWS_SESSION_TOKEN"
-  sensitive    = true
-  value        = "my_value_name"
-  category     = "env"
-  workspace_id = tfe_workspace.terraform-aws-vault-demo.id
-  description  = "AWS Session Token"
-  lifecycle {
-    ignore_changes = [value]
-  }
-}
 
 resource "tfe_variable" "terraform-aws-vault-demo-aws_region" {
   key          = "AWS_REGION"
@@ -433,42 +258,6 @@ resource "tfe_workspace" "tfe-v5-aws-install" {
   }
 }
 
-
-resource "tfe_variable" "tfe-v5-aws-install-aws_access_key_id" {
-  key          = "AWS_ACCESS_KEY_ID"
-  value        = "my_value_name"
-  category     = "env"
-  workspace_id = tfe_workspace.tfe-v5-aws-install.id
-  description  = "AWS Access Key ID"
-  lifecycle {
-    ignore_changes = [value]
-  }
-}
-
-resource "tfe_variable" "tfe-v5-aws-install-aws_secret_access_key" {
-  key          = "AWS_SECRET_ACCESS_KEY"
-  value        = "my_value_name"
-  sensitive    = true
-  category     = "env"
-  workspace_id = tfe_workspace.tfe-v5-aws-install.id
-  description  = "AWS Secret Access Key"
-  lifecycle {
-    ignore_changes = [value]
-  }
-}
-
-resource "tfe_variable" "tfe-v5-aws-install-aws_session_token" {
-  key          = "AWS_SESSION_TOKEN"
-  sensitive    = true
-  value        = "my_value_name"
-  category     = "env"
-  workspace_id = tfe_workspace.tfe-v5-aws-install.id
-  description  = "AWS Session Token"
-  lifecycle {
-    ignore_changes = [value]
-  }
-}
-
 resource "tfe_variable" "tfe-v5-aws-install-aws_region" {
   key          = "AWS_REGION"
   value        = var.aws_default_region
@@ -502,42 +291,6 @@ resource "tfe_workspace" "tfe-v5-aws-prepare" {
     oauth_token_id     = local.tfc_oauth_token
   }
 
-}
-
-
-resource "tfe_variable" "tfe-v5-aws-prepare-aws_access_key_id" {
-  key          = "AWS_ACCESS_KEY_ID"
-  value        = "my_value_name"
-  category     = "env"
-  workspace_id = tfe_workspace.tfe-v5-aws-prepare.id
-  description  = "AWS Access Key ID"
-  lifecycle {
-    ignore_changes = [value]
-  }
-}
-
-resource "tfe_variable" "tfe-v5-aws-prepare-aws_secret_access_key" {
-  key          = "AWS_SECRET_ACCESS_KEY"
-  value        = "my_value_name"
-  sensitive    = true
-  category     = "env"
-  workspace_id = tfe_workspace.tfe-v5-aws-prepare.id
-  description  = "AWS Secret Access Key"
-  lifecycle {
-    ignore_changes = [value]
-  }
-}
-
-resource "tfe_variable" "tfe-v5-aws-prepare-aws_session_token" {
-  key          = "AWS_SESSION_TOKEN"
-  sensitive    = true
-  value        = "my_value_name"
-  category     = "env"
-  workspace_id = tfe_workspace.tfe-v5-aws-prepare.id
-  description  = "AWS Session Token"
-  lifecycle {
-    ignore_changes = [value]
-  }
 }
 
 resource "tfe_variable" "tfe-v5-aws-prepare-aws_region" {
@@ -577,40 +330,6 @@ resource "tfe_workspace" "aws-shared-infra" {
 
 }
 
-resource "tfe_variable" "aws-shared-infra_access_key_id" {
-  key          = "AWS_ACCESS_KEY_ID"
-  value        = "my_value_name"
-  category     = "env"
-  workspace_id = tfe_workspace.aws-shared-infra.id
-  description  = "AWS Access Key ID"
-  lifecycle {
-    ignore_changes = [value]
-  }
-}
-
-resource "tfe_variable" "aws-shared-infra_secret_access_key" {
-  key          = "AWS_SECRET_ACCESS_KEY"
-  value        = "my_value_name"
-  sensitive    = true
-  category     = "env"
-  workspace_id = tfe_workspace.aws-shared-infra.id
-  description  = "AWS Secret Access Key"
-  lifecycle {
-    ignore_changes = [value]
-  }
-}
-
-resource "tfe_variable" "aws-shared-infra_session_token" {
-  key          = "AWS_SESSION_TOKEN"
-  sensitive    = true
-  value        = "my_value_name"
-  category     = "env"
-  workspace_id = tfe_workspace.aws-shared-infra.id
-  description  = "AWS Session Token"
-  lifecycle {
-    ignore_changes = [value]
-  }
-}
 
 resource "tfe_variable" "aws-shared-infra-aws_region" {
   key          = "AWS_REGION"
@@ -648,41 +367,6 @@ resource "tfe_workspace" "multicloud-dns-management" {
 }
 
 
-resource "tfe_variable" "multicloud-dns-management_access_key_id" {
-  key          = "AWS_ACCESS_KEY_ID"
-  value        = "my_value_name"
-  category     = "env"
-  workspace_id = tfe_workspace.multicloud-dns-management.id
-  description  = "AWS Access Key ID"
-  lifecycle {
-    ignore_changes = [value]
-  }
-}
-
-resource "tfe_variable" "multicloud-dns-management_secret_access_key" {
-  key          = "AWS_SECRET_ACCESS_KEY"
-  value        = "my_value_name"
-  sensitive    = true
-  category     = "env"
-  workspace_id = tfe_workspace.multicloud-dns-management.id
-  description  = "AWS Secret Access Key"
-  lifecycle {
-    ignore_changes = [value]
-  }
-}
-
-resource "tfe_variable" "multicloud-dns-management_session_token" {
-  key          = "AWS_SESSION_TOKEN"
-  sensitive    = true
-  value        = "my_value_name"
-  category     = "env"
-  workspace_id = tfe_workspace.multicloud-dns-management.id
-  description  = "AWS Session Token"
-  lifecycle {
-    ignore_changes = [value]
-  }
-}
-
 resource "tfe_variable" "multicloud-dns-management-aws_region" {
   key          = "AWS_REGION"
   value        = var.aws_default_region
@@ -717,41 +401,6 @@ resource "tfe_workspace" "aws-lambda-example" {
   }
 }
 
-
-resource "tfe_variable" "aws-lambda-example_access_key_id" {
-  key          = "AWS_ACCESS_KEY_ID"
-  value        = "my_value_name"
-  category     = "env"
-  workspace_id = tfe_workspace.aws-lambda-example.id
-  description  = "AWS Access Key ID"
-  lifecycle {
-    ignore_changes = [value]
-  }
-}
-
-resource "tfe_variable" "aws-lambda-example_secret_access_key" {
-  key          = "AWS_SECRET_ACCESS_KEY"
-  value        = "my_value_name"
-  sensitive    = true
-  category     = "env"
-  workspace_id = tfe_workspace.aws-lambda-example.id
-  description  = "AWS Secret Access Key"
-  lifecycle {
-    ignore_changes = [value]
-  }
-}
-
-resource "tfe_variable" "aws-lambda-example_session_token" {
-  key          = "AWS_SESSION_TOKEN"
-  sensitive    = true
-  value        = "my_value_name"
-  category     = "env"
-  workspace_id = tfe_workspace.aws-lambda-example.id
-  description  = "AWS Session Token"
-  lifecycle {
-    ignore_changes = [value]
-  }
-}
 
 resource "tfe_variable" "aws-lambda-example-aws_region" {
   key          = "AWS_REGION"
