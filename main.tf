35dd94775744cc41b40f938ceae9a3a9cae3ded7
Yulei Liu <yulei.liu@gmail.com>
2021-10-01 13:51:32 +1000

    test

--- main.tf
+++ main.tf
@@ -375,7 +375,7 @@ resource "tfe_workspace" "aws-shared-infra" {
   execution_mode        = "remote"
   file_triggers_enabled = false
   global_remote_state   = false
-  remote_state_consumer_ids = data.tfe_workspace_ids.aws-internal-apps.ids
+  remote_state_consumer_ids = [data.tfe_workspace_ids.aws-internal-apps.ids]
 
   name = "aws-shared-infra"
 
