ade20b4c8839d21c6661ba6ad60dff78554efdd1
Yulei Liu <yulei.liu@gmail.com>
2021-11-25 10:36:00 +1100

    re-enable gcp-playground

--- main.tf
+++ main.tf
@@ -118,28 +118,28 @@ resource "tfe_workspace" "azure-simple-demo" {
   }
 }
 
-# resource "tfe_workspace" "gcp-playground" {
-#   description                   = "A simple demo of deploying gcp resources."
-#   allow_destroy_plan            = true
-#   auto_apply                    = true
-#   execution_mode                = "remote"
-#   file_triggers_enabled         = false
-#   global_remote_state           = false
-#   name                          = "gcp-playground"
-#   organization                  = var.organization
-#   queue_all_runs                = false
-#   speculative_enabled           = true
-#   structured_run_output_enabled = true
-#   tag_names                     = ["customerfacing", "gcp", "autoinject", "internal", "testing"]
-#   terraform_version             = var.v1latest
-#   trigger_prefixes              = []
+resource "tfe_workspace" "gcp-playground" {
+  description                   = "A simple demo of deploying gcp resources."
+  allow_destroy_plan            = true
+  auto_apply                    = true
+  execution_mode                = "remote"
+  file_triggers_enabled         = false
+  global_remote_state           = false
+  name                          = "gcp-playground"
+  organization                  = var.organization
+  queue_all_runs                = false
+  speculative_enabled           = true
+  structured_run_output_enabled = true
+  tag_names                     = ["customerfacing", "gcp", "autoinject", "internal", "testing"]
+  terraform_version             = var.v1latest
+  trigger_prefixes              = []
 
-#   vcs_repo {
-#     identifier         = "ausmartway/gcp-playground"
-#     ingress_submodules = false
-#     oauth_token_id     = local.tfc_oauth_token
-#   }
-# }
+  vcs_repo {
+    identifier         = "ausmartway/gcp-playground"
+    ingress_submodules = false
+    oauth_token_id     = local.tfc_oauth_token
+  }
+}
 
 
 resource "tfe_workspace" "azure-shared-infra" {
