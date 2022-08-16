4528b4ee71a8184f758229d9913a0aae31d0b0c6
Yulei Liu <yulei.liu@gmail.com>
2022-08-16 17:12:09 +1000

    infracost

--- variables.tf
+++ variables.tf
@@ -52,6 +52,12 @@ variable "snyk_hmac_key" {
   description = "HMAC key for Sync.io integration"
 }
 
+variable "infracost_hmac_key" {
+  type        = string
+  description = "HMAC key for infracost.io integration"
+}
+
+
 variable "organization" {
   type        = string
   description = "Name of TFC/E organization"
