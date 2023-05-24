b4f3bfb15a3568469c8ca55a371a17c5093b5d97
Yulei Liu <yulei.liu@gmail.com>
2023-05-24 15:50:01 +1000

    terraform fmt

--- flexible-workspaces.tf
+++ flexible-workspaces.tf
@@ -220,29 +220,6 @@ resource "tfe_variable" "aws-s3-demo-aws_region" {
   description  = "AWS REGION"
 }
 
-# resource "tfe_workspace" "azure-simple-demo" {
-#   description                   = "A simple demo of deploying Azure resources."
-#   allow_destroy_plan            = true
-#   auto_apply                    = true
-#   execution_mode                = "remote"
-#   file_triggers_enabled         = false
-#   global_remote_state           = false
-#   name                          = "azure-simple-demo"
-#   organization                  = var.organization
-#   queue_all_runs                = false
-#   speculative_enabled           = true
-#   structured_run_output_enabled = true
-#   tag_names                     = ["customerfacing", "azure", "autoinject"]
-#   terraform_version             = var.v1latest
-#   trigger_prefixes              = []
-
-#   vcs_repo {
-#     identifier         = "ausmartway/azure-basic-demo"
-#     ingress_submodules = false
-#     oauth_token_id     = local.tfc_oauth_token
-#   }
-# }
-
 resource "tfe_workspace" "gcp-playground" {
   description                   = "A simple demo of deploying gcp resources."
   allow_destroy_plan            = true
