e275bb45c109fad80b2ffc3d8bbc928caef4ec60
Yulei Liu <yulei.liu@gmail.com>
2024-10-17 17:11:05 +1100

    another test

--- management-settings.tf
+++ management-settings.tf
@@ -2,15 +2,15 @@
 terraform {
   required_version = ">= 1.0"
   required_providers {
-    tfe   = "= 0.59.0"
+    tfe   = ">= 0.58.0"
     vault = "= 4.4.0"
     github = {
       source  = "integrations/github"
-      version = "= 6.3.1"
+      version = ">= 6.0"
     }
     auth0 = {
       source  = "auth0/auth0"
-      version = "~> 0.35.0"
+      version = ">= 0.35.0"
     }
   }
 }
