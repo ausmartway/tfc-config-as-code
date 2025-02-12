da974afc6c6e6acd82d8ac91d4877d38632ef22e
Yulei Liu <yulei.liu@gmail.com>
2025-02-12 18:00:58 +1100

    re-provision gcp-playground ws

--- flexible-workspaces.tf
+++ flexible-workspaces.tf
@@ -221,29 +221,29 @@ resource "tfe_variable" "aws-s3-demo-aws_region" {
   description  = "AWS REGION"
 }
 
-# resource "tfe_workspace" "gcp-playground" {
-#   description        = "A simple demo of deploying gcp resources."
-#   allow_destroy_plan = true
-#   auto_apply         = true
-#   # execution_mode                = "remote"
-#   file_triggers_enabled = false
-#   # global_remote_state           = false
-#   name                          = "gcp-playground"
-#   organization                  = var.organization
-#   queue_all_runs                = false
-#   speculative_enabled           = true
-#   structured_run_output_enabled = true
-#   tag_names                     = ["customerfacing", "gcp", "autoinject", "internal", "testing"]
-#   terraform_version             = var.v1latest
-#   trigger_prefixes              = []
-#   working_directory             = "terraform-standup-gke"
-#   force_delete = true
-#   vcs_repo {
-#     identifier         = "ausmartway/gcp-playground"
-#     ingress_submodules = false
-#     oauth_token_id     = local.tfc_oauth_token
-#   }
-# }
+resource "tfe_workspace" "gcp-playground" {
+  description        = "A simple demo of deploying gcp resources."
+  allow_destroy_plan = true
+  auto_apply         = true
+  # execution_mode                = "remote"
+  file_triggers_enabled = false
+  # global_remote_state           = false
+  name                          = "gcp-playground"
+  organization                  = var.organization
+  queue_all_runs                = false
+  speculative_enabled           = true
+  structured_run_output_enabled = true
+  tag_names                     = ["customerfacing", "gcp", "autoinject", "internal", "testing"]
+  terraform_version             = var.v1latest
+  trigger_prefixes              = []
+  working_directory             = "terraform-standup-gke"
+  force_delete = true
+  vcs_repo {
+    identifier         = "ausmartway/gcp-playground"
+    ingress_submodules = false
+    oauth_token_id     = local.tfc_oauth_token
+  }
+}
 
 //get a list of TFC/E workspaces that has tag 'aws'
 data "tfe_workspace_ids" "awsconsumer-apps" {
