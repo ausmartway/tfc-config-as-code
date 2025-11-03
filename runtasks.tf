54ec2dc3166dd0b14521ba9cb56e97f980cf5aea
Yulei Liu <yulei.liu@gmail.com>
2025-11-03 19:43:59 +1100

    addin packer runtask

---
+++ runtasks.tf
@@ -0,0 +1,6 @@
+resource "tfe_organization_run_task" "hcp-packer" {
+  url          = "https://api.cloud.hashicorp.com/packer/2023-01-01/terraform-cloud/validation/b4b2bfbf-d68e-4bf2-ad12-6213f924d728"
+  name         = "hcp-packer"
+  enabled      = true
+  description  = "hcp-packer runtask"
+}
