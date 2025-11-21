b9075d7b4fcd1f587618c1d92fdc8ab02230923d
Yulei Liu <yulei.liu@gmail.com>
2025-11-21 12:57:03 +1100

    debug

--- management-settings.tf
+++ management-settings.tf
@@ -339,49 +339,6 @@ resource "tfe_variable" "boundary_admin_password" {
 #   path = "kv/azure"
 # }
 
-resource "tfe_variable_set" "enable_workload_identity_for_vault" {
-
-  name = "enable_workload_identity_for_vault"
-  description = "Enable workload identity features for Vault"
-  organization = var.organization
-}
-
-resource "tfe_variable" "tfc_vault_provider_auth" {
-  key             = "tfc_vault_provider_auth"
-  value           = "true"
-  category        = "env"
-  description     = "enable vault provider auth"
-  variable_set_id = tfe_variable_set.enable_workload_identity_for_vault.id
-  
-}
-
-resource "tfe_variable" "tfc_vault_addr" {
-  key             = "tfc_vault_addr"
-  value           = "https://vault-plus-demo-public-vault-16765abc.e222d45b.z1.hashicorp.cloud:8200/"
-  category        = "env"
-  description     = "Vault addr"
-  variable_set_id = tfe_variable_set.enable_workload_identity_for_vault.id
-  
-}
-
-resource "tfe_variable" "tfc_vault_run_role" {
-  key             = "tfc_vault_run_role"
-  value           = "https://vault-plus-demo-public-vault-16765abc.e222d45b.z1.hashicorp.cloud:8200/"
-  category        = "env"
-  description     = "Vault addr"
-  variable_set_id = tfe_variable_set.enable_workload_identity_for_vault.id
-  
-}
-
-resource "tfe_variable" "tfc_vault_namespace" {
-  key             = "tfc_vault_namespace"
-  value           = "admin"
-  category        = "env"
-  description     = "Vault Namespace"
-  variable_set_id = tfe_variable_set.enable_workload_identity_for_vault.id
-  
-}
-
 ## Add Azure credentials ENV variables 
 resource "tfe_variable" "azure_subscription_id" {
   key             = "ARM_SUBSCRIPTION_ID"
